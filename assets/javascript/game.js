ad597e5871adbea15edd352758f4f84a34980c9d
Patrick Lehmann <patlehmann1@gmail.com>
2018-05-13 14:14:48 -0400

    pretty much done with HTML layout

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,7 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h",
 "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
 "t", "u", "v", "w", "x", "y", "z"];
 
-var 
+var turnsUsed  // number of turns used
+var lettersUsed // letters already used
+var wordsUsed // words to be guessed in game
+
