27f8ac14eacb6d7e86a10311c694d9e625333b3b
Patrick Lehmann <patlehmann1@gmail.com>
2018-05-16 23:30:32 -0400

    i'm trying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,75 @@
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h",
-"i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-"t", "u", "v", "w", "x", "y", "z"];
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H",
+"I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+"T", "U", "V", "W", "X", "Y", "Z"];
+
+var wins = 0;
+var guessesRemain = 15;
 
 var turnsUsed  // number of turns used
 var lettersUsed // letters already used
 var wordsUsed // words to be guessed in game
+var currentWordindex // index of the current word in the array
+var guessedLetters = [];
+var guessingWord = [];
+var gameStart = false;
 
-var words = [
+
+var islanderWords = [
     "BOSSY",
     "NYSTROM",
     "TAVARES",
-    "LONGISLAND",
-    "BROOKLYN",
+    "TROTTIER",
+    "SMITH",
     "HOCKEY",
-    "STANLEYCUP"
+    "STANLEYCUP",
+    "DEFENSEMAN",
+    "FORWARD",
+    "GOALIE",
+    "COLISEUM"
 ];
 
-var word = words[Math.floor(Math.random() * words.length)];
+var word = islanderWords[Math.floor(Math.random() * islanderWords.length)];
+
+currentWordIndex = Math.floor(Math.random() * (islanderWords.length));
+
+var guessArray = [];
+for (var i = 0; i < word.length; i++) {
+    guessArray.push("_ ");
+}
 
 var answerArray = [];
-for (var i = 0; i < word.length; i++) {
- answerArray[i] = "_ ";
+for (var j = 0; j < word.length; j++) {
+    answerArray.push(word[j]);
 }
 
 var remainingLetters = word.length;
 
-window.onload = function(){
-    document.getElementById("guessedWords").textContent = answerArray.join("");
+function replaceWords(userGuess){
+    var str = guessArray[i];
+    var ans = str.replace(answerArray[currentWordindex]);
 }
 
-console.log(answerArray.join(""));
+function makeGuess(letter) {
+    if (guessedLetters.indexOf(letter) === -1) {
+        guessedLetters.push(letter);
+        evaluateGuess(letter);
+    }
+
+}
+
+
+window.onload = function gameStart(){
+    document.getElementById("guessedWords").textContent = guessArray.join("");
+    document.getElementById("wins").textContent = wins;
+    document.getElementById("guessesRemain").textContent = guessesRemain;
+}
+
+document.onkeyup = function(event){
+    
+    var userGuess = event.keyCode;
+
+    if (userGuess >= 65 && userGuess <= 90){
+        console.log("I'm having trouble");
+            }
+    }
+
