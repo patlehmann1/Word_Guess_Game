4535d2cd0c731723877810b08d8a31c1c2c7948d
Patrick Lehmann <patlehmann1@gmail.com>
2018-05-18 23:25:36 -0400

    going to sleep

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var letters = ["A", "B", "C", "D", "E", "F", "G", "H",
 "T", "U", "V", "W", "X", "Y", "Z"];
 
 var wins = 0;
-var guessesRemain = 15;
+var guessesRemain = 20;
 
 var turnsUsed  // number of turns used
 var lettersUsed // letters already used
@@ -12,6 +12,7 @@ var currentWordindex // index of the current word in the array
 var guessedLetters = [];
 var guessingWord = [];
 var gameStart = false;
+
 
 var guessedLettersUp = guessedLetters.toUpperCase;
 
@@ -41,7 +42,7 @@ function getAllIndexes(array, letter) {
     var indexes = [];
     var i = -1;
     while ((i = array.indexOf(letter, i + 1)) != -1) {
-        indexes.push(i);
+        indexes.push(array[i]);
     }
     return indexes;
 }
@@ -60,20 +61,29 @@ currentWordIndex = Math.floor(Math.random() * (islanderWords.length));
 
 var remainingLetters = word.length;
 
-document.onkeyup = function gamePlay(event){
+document.onkeyup = function gameStart(){
 
     document.getElementById("guessedWords").textContent = guessArray.join("");
     document.getElementById("wins").textContent = wins;
     document.getElementById("guessesRemain").textContent = guessesRemain;    
 
-var userGuessNum = event.keyCode;
-var userGuess = event.key;
+    var userGuessNum = event.keyCode;
+    var userGuess = event.key;
 
     if (userGuessNum >= 65 && userGuessNum <= 90){
-        guessedLetters.toUpperCase;
+
         guessedLetters.push(userGuess);
+
         removeDuplicates(guessedLetters);
+
         document.getElementById("guessed-letters").textContent = guessedLetters;
-        guessesRemain--;
+    
+        if (guessesRemain > 0){
+            guessesRemain--;
+        }
+        else {
+        alert("YOU LOSE!!");
+    }
+
 }
 }
